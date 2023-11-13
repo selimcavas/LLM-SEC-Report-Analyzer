f856729051f5803f4ca5f835a2df6d0355a625dd
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-11-13 16:01:15 +0300

    modified tool to use gpt4

--- tool/tools.py
+++ tool/tools.py
@@ -1,24 +1,22 @@
 from dotenv import load_dotenv
-from langchain.agents import create_csv_agent, tool
+from langchain.agents import create_csv_agent, tool, AgentType
 from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 import os
 
-from regex import S
-
 load_dotenv()
+
 
 @tool
 def csv_agent_tool(prompt: str) -> str:
     """Used to analyze CSV files."""
-    agent = create_csv_agent(
-        llm = ChatOpenAI(
-        openai_api_key= os.getenv('OPENAI_API_KEY'),
-        model="gpt-3.5-turbo",
-        temperature=0,
-    ), 
 
-    max_iterations=5,
-    verbose=True,
+    agent = create_csv_agent(
+        path="combined_data.csv",
+        llm=OpenAI(temperature=0),
+        max_iterations=5,
+        verbose=True,
+
     )
 
     return agent(prompt)
