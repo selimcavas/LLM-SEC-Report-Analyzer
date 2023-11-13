e5ee37e4f8ce5f5f4cc464ee7ceeec8f17ca0852
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-11-13 16:01:04 +0300

    modified tool description and prefix

--- agent/agent.py
+++ agent/agent.py
@@ -3,23 +3,31 @@ from dotenv import load_dotenv
 from langchain.agents import Tool, ZeroShotAgent, AgentExecutor
 from langchain.memory import ConversationBufferMemory
 from langchain import LLMChain
-from langchain.chat_models  import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 from tool.tools import csv_agent_tool
 import os
 
 load_dotenv()
 
+
 def run_main_agent(user_question):
 
     tool_list = []
 
-    csv_tool = Tool(name="csv_tool", func=csv_agent_tool, description="This tool will be used to answer questions about the data inside CSV files.")
+    csv_tool = Tool(name="csv_tool", func=csv_agent_tool,
+                    description='''Use this tool when user asks for a field value from a csv file. 
+                    Input should be a single string in JSON format. 
+                    ''')
 
     tool_list.append(csv_tool)
 
     prefix = """
     You are a financial data informant designed to chat with investors. 
-    You will be provided with csv files regarding company's quarterly financial data from 10-Q filings.
+    You will be provided with a csv file called combined_data.csv regarding company's quarterly financial data from 10-Q filings. 
+    The csv file has columns that represent the company ticker, field, and quarters.
+    Field name format is pascal case with spaces between words, however some fields have abbreviations, if you cannot find that field try to make all letters uppercase, it can sometimes be mixed as well like 'Basic EPS'.
+    Quarter column names are formatted like this year-quarter_number for example '2023-Q3'.
+    If NaN values exists remove them before you do any mathematical calculation.
     Answer the following questions as best you can, and you have access to following tools.
     If you are asked to provide invesment idea or make suggestions, kindly respond to user saying that you are unable to make investment decisions since you are only a chatbot. 
     """
@@ -29,8 +37,9 @@ def run_main_agent(user_question):
         {chat_history}
         Question: {input} 
         {agent_scratchpad} """
-    
-    prompt = ZeroShotAgent.create_prompt(tool_list, prefix=prefix, suffix=suffix, input_variables=["input", "chat_history", "agent_scratchpad"])
+
+    prompt = ZeroShotAgent.create_prompt(tool_list, prefix=prefix, suffix=suffix, input_variables=[
+                                         "input", "chat_history", "agent_scratchpad"])
     memory = ConversationBufferMemory(memory_key="chat_history")
     llm_chain = LLMChain(
         llm=ChatOpenAI(
