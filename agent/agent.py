cae5e54cbd8b6ea2a9a43d1b571ba277050accb8
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-11-18 13:59:23 +0300

    modified agent and removed unused files

--- agent/agent.py
+++ agent/agent.py
@@ -15,7 +15,9 @@ def run_main_agent(user_question):
     tool_list = []
 
     csv_tool = Tool(name="csv_tool", func=csv_agent_tool,
-                    description='''Use this tool when user asks for a field value from a csv file. 
+                    description='''Use this tool when user asks for a field value from a csv file or when asked for complex data analysis.
+                    For complex data analysis, the tool will return the results in a tabular form with columns for 'Ticker', 'Field', and 'Value'.
+                    When asked for complex data analysis such as finding the top 5 companies with the maximum income, present the results in a DataFrame with columns for 'Ticker', 'Field', and 'Value'.
                     Input should be a single string in JSON format. 
                     ''')
 
@@ -24,12 +26,13 @@ def run_main_agent(user_question):
     prefix = """
     You are a financial data informant designed to chat with investors. 
     You will be provided with a csv file called combined_data.csv regarding company's quarterly financial data from 10-Q filings. 
-    The csv file has columns that represent the company ticker, field, and quarters.
-    Field name format is pascal case with spaces between words, however some fields have abbreviations, if you cannot find that field try to make all letters uppercase, it can sometimes be mixed as well like 'Basic EPS'.
+    The csv file has columns that represent the company ticker, field, and quarters. Each company has a unique ticker and they have individual rows for each field.
+    Field name format is pascal case with spaces between words, however some fields have abbreviations, if you cannot find that field try to make all letters uppercase or use plural form of words, it can sometimes be mixed as well like 'Basic EPS'.
     Quarter column names are formatted like this year-quarter_number for example '2023-Q3'.
     If NaN values exists remove them before you do any mathematical calculation.
     Answer the following questions as best you can, and you have access to following tools.
     If you are asked to provide invesment idea or make suggestions, kindly respond to user saying that you are unable to make investment decisions since you are only a chatbot. 
+
     """
 
     suffix = """ Begin! 
