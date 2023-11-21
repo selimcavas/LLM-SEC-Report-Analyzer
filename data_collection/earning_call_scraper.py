700d91b0351f7e43373e0c6582533d6dac3a69e2
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-11-21 13:38:47 +0300

    added part to extract info from links

--- data_collection/earning_call_scraper.py
+++ data_collection/earning_call_scraper.py
@@ -1,3 +1,4 @@
+import re
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
@@ -28,42 +29,67 @@ for ticker in tickers:
     url = base_url + ticker.lower() + "/#quote-earnings-transcripts"
     urls.append(url)
 
+with open('earning_call_links.txt', 'r') as file:
+    earning_call_links = file.read().splitlines()
 
-with open('earning_call_links.txt', 'w') as file:
+with open('earning_call_tickers.txt', 'w') as file:
+    for link in earning_call_links:
+        link = link.rstrip('/')
+
+        # Split the URL into parts based on the '/' character
+        parts = link.split('/')
+
+        # The part we're interested in is the last part
+        last_part = parts[-1]
+
+        # Split the last part into subparts based on the '-' character
+        subparts = last_part.split('-')
+
+        # The ticker, quarter, and year are the first, second, and third subparts, respectively
+        ticker = subparts[-6]
+        quarter = subparts[-5]
+        year = subparts[-4]
+
+        print(f"Ticker: {ticker}, Quarter: {quarter}, Year: {year}")
+
+        # Write the ticker, quarter, and year to the text file
+        file.write(f"{ticker},{quarter},{year}\n")
+
+# with open('earning_call_links.txt', 'w') as file:
     # Print the URLs
-    for url in urls:
-        driver.get(url)
+    # for url in urls:
+    # driver.get(url)
+    # sleep(3)
+    # extracting earning call links
+    # if url == urls[0]:
+    #     reject_cookies_xpath = """//*[@id="onetrust-reject-all-handler"]"""
+    #     # click on the reject cookies button
+    #     driver.find_element(By.XPATH, reject_cookies_xpath).click()
 
-        sleep(3)
+    # for i in range(1, 5):
+    #     # Construct the XPath string
+    #     xpath_a = base_xpath_earning_call_a + str(i) + "]"
+    #     xpath_b = base_xpath_earning_call_b_plan + str(i) + "]"
 
-        if url == urls[0]:
-            reject_cookies_xpath = """//*[@id="onetrust-reject-all-handler"]"""
-            # click on the reject cookies button
-            driver.find_element(By.XPATH, reject_cookies_xpath).click()
+    #     # Wait for the element to be present and then select it
+    #     try:
+    #         element = WebDriverWait(driver, 5).until(
+    #             EC.presence_of_element_located((By.XPATH, xpath_a)))
+    #     except:
+    #         try:
+    #             element = WebDriverWait(driver, 5).until(
+    #                 EC.presence_of_element_located((By.XPATH, xpath_b)))
+    #         except:
+    #             print(
+    #                 f"Element not found for index {i}. Skipping to next index.")
+    #             continue
 
-        for i in range(1, 5):
-            # Construct the XPath string
-            xpath_a = base_xpath_earning_call_a + str(i) + "]"
-            xpath_b = base_xpath_earning_call_b_plan + str(i) + "]"
+    #     # Get the href attribute of the element
+    #     link = element.get_attribute('href')
 
-            # Wait for the element to be present and then select it
-            try:
-                element = WebDriverWait(driver, 5).until(
-                    EC.presence_of_element_located((By.XPATH, xpath_a)))
-            except:
-                try:
-                    element = WebDriverWait(driver, 5).until(
-                        EC.presence_of_element_located((By.XPATH, xpath_b)))
-                except:
-                    print(
-                        f"Element not found for index {i}. Skipping to next index.")
-                    continue
+    #     # Write the link to the text file
+    #     file.write(link + '\n')
 
-            # Get the href attribute of the element
-            link = element.get_attribute('href')
-
-            # Write the link to the text file
-            file.write(link + '\n')
 
 # Close the WebDriver
-driver.quit()
+# driver.quit()
