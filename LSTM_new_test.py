ec1dca295d1812f5d9e133ebc457b553aff47df2
Selim Cavas <selim.cavas@ozu.edu.tr>
2024-04-29 21:30:46 +0300

    added lstm test filfe

--- LSTM_new_test.py
+++ LSTM_new_test.py
@@ -5,20 +5,116 @@ from datetime import datetime
 import datetime as dt
 import pandas as pd
 import numpy as np
+import os
 
-
-from keras.preprocessing.sequence import TimeseriesGenerator
-
-from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers import Dense
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
 from sklearn.preprocessing import MinMaxScaler
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from keras.metrics import RootMeanSquaredError
+from tensorflow.keras.metrics import RootMeanSquaredError
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
 
-def stock_prices_predictor_tool(days: str, ticker: str) -> str:
+
+def train_and_save_model(ticker):
+
+    if os.path.isfile(f'models/{ticker}.keras'):
+        print(f'{ticker} model already exists passing.')
+        return
+
+    # Connect to the SQLite database
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    # Calculate the start date for the last 1 year of data
+    # Prepare the SQL query to get the last record date
+    sql_query_last_date = '''
+        SELECT MAX(date)
+        FROM stock_prices
+        WHERE ticker = ?
+    '''
+
+    # Execute the SQL query
+    c.execute(sql_query_last_date, (ticker,))
+    end_date = c.fetchall()[0][0]
+
+    print(f"🟢 Last date: {end_date}")
+
+    if end_date == None:
+        print(f'{ticker} end date does not exist passing.')
+        return
+
+    # Convert the end_date to a datetime object
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    start_date = end_date - relativedelta(years=2)
+
+    start_date_str = start_date.strftime('%Y-%m-%d')
+    end_date_str = end_date.strftime('%Y-%m-%d')
+
+    print(f"🟢 Start date: {start_date_str}, End date: {end_date_str}")
+
+    # Prepare the SQL query
+    sql_query = '''
+        SELECT date, price, volume
+        FROM stock_prices
+        WHERE date BETWEEN ? AND ? AND ticker = ?
+        ORDER BY date
+    '''
+
+    # Execute the SQL query
+    c.execute(sql_query, (start_date_str, end_date_str, ticker))
+    rows = c.fetchall()
+
+    # Check if any data was fetched
+    if not rows:
+        return f'No data for {ticker} between {start_date_str} and {end_date_str}'
+
+    # Convert the data to a pandas DataFrame
+    df = pd.DataFrame(rows, columns=['date', 'price', 'volume'])
+
+    df['price_diff'] = df['price'].diff()
+
+    df.drop(0, inplace=True)
+    df.drop('date', axis=1, inplace=True)
+
+    win_length = 5  # window length 5 days
+    batch_size = 15  # train
+    n_features = 3  # number of features
+
+    scaler = MinMaxScaler()
+    scaled_data = scaler.fit_transform(df)
+
+    features = scaled_data
+    target = scaled_data[:, 0]
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        features, target, test_size=0.2, random_state=42, shuffle=False)
+
+    train_generator = TimeseriesGenerator(
+        x_train, y_train, length=win_length, sampling_rate=1, batch_size=batch_size)
+    test_generator = TimeseriesGenerator(
+        x_test, y_test, length=win_length, sampling_rate=1, batch_size=batch_size)
+
+    model = Sequential()
+    model.add(LSTM(128, activation='relu', input_shape=(
+        win_length, n_features), return_sequences=False))
+    model.add(Dense(1))
+
+    model.compile(optimizer='adam', loss='mse')
+
+    model.fit(train_generator, epochs=30,
+              validation_data=test_generator, shuffle=False)
+
+    print(model.evaluate(test_generator))
+
+    model.save(f'models/{ticker}.keras')
+
+
+def stock_prices_predictor_tool(days, ticker):
     # Connect to the SQLite database
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
@@ -67,100 +163,49 @@ def stock_prices_predictor_tool(days: str, ticker: str) -> str:
 
     df['price_diff'] = df['price'].diff()
 
-    # Calculate the sentiment score
-    df['sentiment_score'] = np.sign(df['price_diff']).fillna(0).astype(int)
+    df.drop(0, inplace=True)
+    df.drop('date', axis=1, inplace=True)
 
-    print(f"🟢 Fetched data: {df}")
-
-    # Preprocess the data for the LSTM model
     scaler = MinMaxScaler()
-    scaled_data = scaler.fit_transform(df[['volume', 'sentiment_score', 'price_diff']])
+    scaled_data = scaler.fit_transform(df)
 
-    n_input = 15
-    forward_days = min(int(days), 7)  # User input for number of days, max 7
-    n_features = 3  # Adjusted for the new dataframe with added columns
+    features = scaled_data
+    target = scaled_data[:, 0]
 
+    win_length = 5  # window length 5 days
+    batch_size = 15  # train
+    n_features = 3  # number of features
 
-    # Split the data into train and test sets
-    train_data, test_data = train_test_split(scaled_data, test_size=0.1, shuffle=False)
-
-    # Create generators for the train and test sets
-    train_generator = TimeseriesGenerator(train_data, train_data, length=n_input, batch_size=20)
-    test_generator = TimeseriesGenerator(test_data, test_data, length=n_input, batch_size=1)
-
-    # Define the LSTM model
-    model = Sequential()
-
-    model.add(LSTM(128, activation='relu', input_shape=(n_input, n_features), return_sequences=True))
-    model.add(LSTM(128, activation='relu', return_sequences=True))
-    model.add(LSTM(128, activation='relu', return_sequences=False))
-    model.add(Dense(n_features))
-    model.compile(optimizer='adam', loss=RootMeanSquaredError())
-
-    model.fit(train_generator, epochs=30, validation_data=test_generator)
-
-
-    # Use the model to predict future stock prices
     pred_list = []
+    batch = scaled_data[-win_length:].reshape((1, win_length, n_features))
+    print(batch)
 
-    current_batch = scaled_data[-n_input:].reshape((1, n_input, n_features))
+    model = load_model(f'models/{ticker}.keras')
 
-    print(f"🟢 current_batch: {current_batch}")
+    for i in range(days):  # assuming days is the number of days into the future you want to predict
+        current_pred = model.predict(batch)[0]
+        print(f"🟢 current pred: {current_pred}")
+        print(f"🟢 current pred price: {current_pred[0]}")
+        # Append only the first element of current_pred
+        pred_list.append(current_pred[0])
+        current_pred = np.repeat(
+            current_pred, n_features).reshape((1, 1, n_features))
+        print(f"🟢 batch: {batch[:, 1:, :]}")
+        print(f"🟢 current pred reshaped: {current_pred}")
+        batch = np.append(batch[:, 1:, :], current_pred, axis=1)
 
-    for i in range(forward_days):
-        current_pred = model.predict(current_batch)[0]
-        print(f"🟢 Current pred: {current_pred}")
-        pred_list.append(current_pred)
-
-        current_batch = np.append(current_batch[:, 1:, :], [
-                                  [current_pred]], axis=1)
-
+    # Convert pred_list to a numpy array and repeat each prediction 3 times
+    pred_array = np.repeat(np.array(pred_list), 3).reshape(-1, 3)
     # Inverse transform the predicted data
-    predicted_prices = scaler.inverse_transform(
-        np.array(pred_list).reshape(-1, 1))
+    inverse = scaler.inverse_transform(pred_array)
+    # Take the first column of inverse as the predictions
+    price_preds = inverse[:, 0]
+    print(f"🟢 predictions: {price_preds}")
 
-    # Generate predicted_dates
-    last_date = datetime.strptime(end_date_str, '%Y-%m-%d')
-    predicted_dates = [(last_date + dt.timedelta(days=i+1)
-                        ).strftime('%Y-%m-%d') for i in range(forward_days)]
 
-    # Append predicted prices and dates to rows
-    for date, price in zip(predicted_dates, predicted_prices.flatten()):
-        rows.append((date, price))
+stock_prices_predictor_tool(3, 'AMZN')
+""" with open('tickers_test.txt', 'r') as f:
+    tickers = f.read().splitlines()
 
-    # Close the connection
-    conn.close()
-
-    # Convert the fetched data and predicted data to a list of tuples
-    actual_data = list(zip(df['date'].values, df['price'].values))
-    predicted_data = list(zip(predicted_dates, predicted_prices.flatten()))
-
-    # Combine the actual and predicted data
-    output = actual_data + predicted_data
-
-    # Convert the lists to DataFrames
-    actual_data_df = pd.DataFrame(actual_data, columns=['date', 'price'])
-    predicted_data_df = pd.DataFrame(predicted_data, columns=['date', 'price'])
-
-    # Calculate the price change
-    price_change = (predicted_data_df['price'].values[-1] -
-                    actual_data_df['price'].values[-1]) / actual_data_df['price'].values[-1] * 100
-
-    # Get the last actual and predicted dates and prices
-    last_actual_date = actual_data_df['date'].values[-1]
-    last_predicted_date = predicted_data_df['date'].values[-1]
-
-    last_actual_price = actual_data_df['price'].values[-1]
-    last_predicted_price = predicted_data_df['price'].values[-1]
-
-    
-    # Convert the output to a DataFrame
-    output_df = pd.DataFrame(output, columns=['date', 'prices'])
-
-    # Convert the 'date' column to datetime
-    output_df['date'] = pd.to_datetime(output_df['date'])
-
-    # Set the 'date' column as the index
-    output_df.set_index('date', inplace=True)
-
-    return output_df
+for ticker in tickers:
+    train_and_save_model(ticker) """
