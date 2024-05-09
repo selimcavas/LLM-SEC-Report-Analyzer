a86ac7ce61ccb0c1564ee55ee9196a857ee4c16b
Selim Çavaş <selim.cavas@ozu.edu.tr>
2024-05-09 15:04:57 +0300

    updated init pinecone and fixed transcript samples

--- init_pinecone.py
+++ init_pinecone.py
@@ -5,8 +5,8 @@ import pinecone
 from pathlib import Path
 from langchain.docstore.document import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
 
 load_dotenv()
 print("initalize_pinecone2")
@@ -41,33 +41,45 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=2000, chunk_overlap=200)
 texts = text_splitter.split_documents(documents)
 
-print('text', texts[0])
+# print('text', texts[0])
 
 # PINECONE SETUP
 
-pinecone.init(
-    api_key=pinecone_api_key,
-    environment=pinecone_env
-)
-
-# Set the index name for this project in pinecone first
-
 index_name = 'sec-filing-analyzer'
-if index_name not in pinecone.list_indexes():
-    pinecone.create_index(name=index_name, dimension=1536, metric="cosine")
-
-# Examine pinecone index. Delete all vectors, if you want to start fresh
-
-index = pinecone.Index(index_name)
-print(index.describe_index_stats())
-# index.delete(deleteAll='true', namespace='')
 
 embeddings = OpenAIEmbeddings(
     openai_api_key=os.environ.get("OPENAI_API_KEY"))
 
-# Create the vector store from the texts
+pc = PineconeVectorStore(
+    index_name=index_name,
+    pinecone_api_key=pinecone_api_key,
+    embedding=embeddings,
+    # namespace=pinecone_env
+)
 
-docsearch = Pinecone.from_documents(
-    texts, embeddings, index_name=index_name)
+docsearch = pc.from_documents(texts, embeddings, index_name=index_name)
 
-# for existing an vector store, use Pinecone.from_existing_index(index_name, embeddings)
+# Set the index name for this project in pinecone first
+
+# index_name = 'sec-filing-analyzer'
+
+# index_names = [index.name for index in pc.list_indexes()]
+
+# print('index names: ', index_names)
+# if index_name not in index_names:
+#     pc.create_index(name=index_name, dimension=1536, metric="cosine")
+
+# # Examine pinecone index. Delete all vectors, if you want to start fresh
+
+# index = pc.Index(index_name)
+# print(index.describe_index_stats())
+
+# embeddings = OpenAIEmbeddings(
+#     openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+# # Create the vector store from the texts
+
+# docsearch = pc.from_documents(
+#     texts, embeddings, index_name=index_name)
+
+# # for existing an vector store, use Pinecone.from_existing_index(index_name, embeddings)
